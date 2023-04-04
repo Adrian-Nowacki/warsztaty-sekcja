c4b2a7001e3a524709154c1c51428cd48ff5c760
anowacki <adriannowacki007@gmail.com>
2023-04-04 14:43:43 +0200

    zakomentowanie

--- script.R
+++ script.R
@@ -1,2 +1,16 @@
-setwd("C:/Users/adria/Desktop/warsztaty_sekcja/warsztaty-sekcja/dane")
 
+
+# ustawcie katalog roboczy na folder z plikami
+# zainstalujcie pakiety sf, tmap i raster
+library(sf)
+library(tmap)
+library(raster)
+
+kawiarnie <- st_read("dane/kawiarnie.gpkg")
+parki <- st_read("dane/parki.gpkg")
+poznan <- raster("dane/poznan_temperatura.tif")
+
+tmap_mode("view")
+tm_shape(kawiarnie) + tm_dots(col = "#222222", size = 0.1) +
+  tm_shape(parki) + tm_polygons(col = "darkgreen") +
+  tm_shape(poznan) + tm_raster(palette = "-RdYlGn")
--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-var map = L.map('map').setView([52.4, 16.9], 12);
+
+// stworzenie mapy
+
+var map = L.map( 'map', {
+    center: [52.4, 16.9],   // współrzędne Poznania
+    minZoom: 7,     //minimalny, maksymalny oraz defaultowy zoom
+    maxZoom: 25,
+    zoom: 12,
+    zIndex: 1,  // kolejnosc wyswietlania warstwy - z index-1 znajduje się wyżej nad warstwą z z-index 0 itd.
+    zoomSnap:0.001,     //delikatnosc przyblizania mapy
+  });
 
 
 
 
-// mapy bazowe
+
+// dodanie podkładów mapowych
 
 var openstreetmap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+    // podkłady mapowe z leaflet basemaps providers
 var leaflet = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
@@ -24,37 +37,48 @@ var satelita = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services
 
 
 
-var temperatura = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
-layers: 'temperatura_poznan',
-format: 'image/png',
-transparent: true,
-opacity:1,
+
+
+// dodanie warstw rastrowych jako wms
+// warstwa temperatury została zakomentowana wszędzie, aby strona się poprawnie wyświetliła
+/*
+var temperatura = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {  // link do zasobów warstw wms, ta akurat jest z lokalnego geoservera, więc nie wyświetli się
+    layers: 'temperatura_poznan',   //nazwa warstwy
+    format: 'image/png',    //format warstwy
+    transparent: true,  //przezroczystosc wartosc irastra równych 0
+    opacity:1,  //nieprzezroczystosc całej warestwy
+}).addTo(map)
+*/
+
+var ndvi = L.tileLayer.wms("http://wms2.geopoz.poznan.pl/geoserver/satelitarne/wms??service?", {        //warstwa z ndvi wyświetli się, ponieważ jest udostępniona publicznie na SIP Poznań
+    layers: 'sentinel_2018_07_15_ndvi',
+    format: 'image/png',
+    transparent: true,
+    opacity:1,
 }).addTo(map)
 
-var ndvi = L.tileLayer.wms("http://wms2.geopoz.poznan.pl/geoserver/satelitarne/wms??service?", {
-layers: 'sentinel_2018_07_15_ndvi',
-format: 'image/png',
-transparent: true,
-opacity:1,
-}).addTo(map)
 
 
-//warstwy wektorowe
+
+
+
+
+// dodanie warstw wektorowych
 
 var parki = new L.geoJSON(parki_2, {onEachFeature:function forEachFeature (feature,layer){
-    if (feature.properties.name != null){
-        layer.bindPopup('<h3>'+feature.properties.name +'</h3>' + '<b> Powierzchnia: </b>' + feature.properties.area + ' ha' +
+    if (feature.properties.name != null){       // tutaj mozna użyć innego warunku, ten jednak jest najpewniejszy, ponieważ najpewniej każdy park ma nazwę (jeśli park posiada nazwę, wtedy to i to)
+        layer.bindPopup('<h3>'+feature.properties.name +'</h3>' + '<b> Powierzchnia: </b>' + feature.properties.area + ' ha' +      //wywołanie popupu ze zdefiniowanym tekstem - nazwy parku oraz jego powierzchni
         '<img style = "border:1px solid #222222; margin-top:10px;" src ="dane/cytadela.jpg" width = "300px" height = "150px"/>'
         )
-        layer.on('click', function (e) {
+        layer.on('click', function (e) {    //zdefiniowanie otworzenia popupu i ustawienia widoku po kliknięcie w daną warstwę
             this.openPopup();
             map.setView(e.latlng, 15);
         });
-        layer.on('clickout', function (e) {
+        layer.on('clickout', function (e) { //zdefiniowanie zamknięcia popupu po nakliknięciu poza warstwą
             this.closePopup();
         });
             }},
-            style:function style(feature) {
+            style:function style(feature) { //ustawienie stylu warstwy
                 return {
                     fillColor: '#3c6e4f',
                     weight: 1,
@@ -67,7 +91,8 @@ var parki = new L.geoJSON(parki_2, {onEachFeature:function forEachFeature (featu
 }).addTo(map)
 
 
-// ikona kawiarni
+
+// zdefiniowanie ikony kawiarni pobranej ze strony flaticon - najpiew należy ją zdefiniować, potem podłączyć poniżej w warstwie kawiarni
 var ikona = L.icon({
     iconUrl: 'dane/cafe_1.png',
     iconSize:     [38, 38], 
@@ -77,9 +102,7 @@ var ikona = L.icon({
     popupAnchor:  [0, -20] 
 });
 
-
-
-var kawiarnie = new L.geoJSON(kawiarnie, {onEachFeature:function forEachFeature (feature,layer){
+var kawiarnie = new L.geoJSON(kawiarnie, {onEachFeature:function forEachFeature (feature,layer){    //tak samo jak w przypadku parków, brakuje jedynie stylu, ponieważ to warstwa puktowa
     if (feature.properties.name != null){
         layer.bindPopup('<h3>'+feature.properties.name +'</h3>')
         layer.on('click', function (e) {
@@ -89,7 +112,7 @@ var kawiarnie = new L.geoJSON(kawiarnie, {onEachFeature:function forEachFeature 
             this.closePopup();
         });
             }},
-            pointToLayer: function(feature,latlng){
+            pointToLayer: function(feature,latlng){     //podłączenie ikony kawiarni
                 return L.marker(latlng,{icon: ikona});
             }
     
@@ -101,14 +124,18 @@ var kawiarnie = new L.geoJSON(kawiarnie, {onEachFeature:function forEachFeature 
 
 
 
+// panel z kontrolami warstw i podkładów mapowych
+
+//podkłady mapowe
 var baseMaps = {
-    "OpenStreetMap": openstreetmap,
+    "OpenStreetMap": openstreetmap,     //"nazwa wyświetlana": zmienna z warstwą/mapą
     "Satelita": satelita,
     "Leaflet": leaflet
 }
 
+// warstwy
 var overlayMaps = {
-    "Temperatura": temperatura,
+    //"Temperatura": temperatura, 
     "NDVI": ndvi,
     "Parki": parki,
     "Kawiarnie": kawiarnie
@@ -117,11 +144,18 @@ var overlayMaps = {
 var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 
+
+
+
+
+
+// proste wyświetlanie divu "container" po nakliknięciu na warstwę parków
+
 parki.on('click', function(){
     document.getElementById("container").style.display = "block";
 })
 
-
+//chowanie divu po nakliknięciu poza warstwą, na mapę
 map.on('click', function(){
     document.getElementById("container").style.display = "none";
 })
